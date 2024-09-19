00389aec07f95b2cc46a1a9e2ddd783b64830d31
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-09-19 20:51:20 +0530

    19-09-2024

---
+++ lib/view/screen/home/componects/appbar_action.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+List<Widget> actionWidget =[
+  Padding(
+    padding: EdgeInsets.all(14.h),
+    child: Row(
+      children: [
+        const Icon(
+          Icons.location_on,
+          color: Color(0xff637394),
+        ),
+        SizedBox(
+          width: 5.w,
+        ),
+        Text(
+          'Nur-Sultan',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14.sp,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
+    ),
+  ),
+  Padding(
+    padding: EdgeInsets.all(8.h),
+    child: Row(
+      children: [
+        const Icon(
+          Icons.language,
+          color: Color(0xff637394),
+        ),
+        SizedBox(
+          width: 5.w,
+        ),
+        Text(
+          'Eng',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14.sp,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ],
+    ),
+  ),
+  Padding(
+    padding: EdgeInsets.all(14.h),
+    child: Container(
+      width: 70.w,
+      alignment: Alignment.center,
+      decoration: ShapeDecoration(
+        gradient: const LinearGradient(
+          begin: Alignment(0.00, -1.00),
+          end: Alignment(0, 1),
+          colors: [Color(0xFFFF8036), Color(0xFFFC6C19)],
+        ),
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8)),
+        shadows: const [
+          BoxShadow(
+            color: Color(0x3FFF8036),
+            blurRadius: 16,
+            offset: Offset(0, 4),
+            spreadRadius: 0,
+          )
+        ],
+      ),
+      child: Text(
+        'Log in',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14.sp,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+    ),
+  ),
+];
