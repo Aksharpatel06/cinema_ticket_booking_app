b19975279f5925b747bd9c42090c40ab4baa74fa
Aksharpatel06 <aksharpatel030306@gmail.com>
2024-09-19 20:58:46 +0530

    19-09-2024

--- lib/view/screen/home/home_page.dart
+++ lib/view/screen/home/home_page.dart
@@ -14,13 +14,39 @@ class HomePage extends StatelessWidget {
         backgroundColor: const Color(0xff1d273a),
         elevation: 1,
         toolbarHeight: 75,
+        leadingWidth: 60,
         leading: Padding(
-          padding: EdgeInsets.all(6.h),
+          padding: EdgeInsets.only(left: 12.h,top: 8.h,bottom: 8.h),
           child: Image.asset(
             'asset/splash/splash_img.png',
           ),
         ),
         actions: actionWidget,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.h),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Now in cinemas',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Icon(
+                  Icons.search_rounded,
+                  size: 28.h,
+                  color: Color(0xff637394),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
